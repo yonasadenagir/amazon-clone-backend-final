d0ae9d2e2daf0976a2431ec0d1889a5c3eb1eaa6
yonasadenagir <yonasadenagir@gmail.com>
2023-12-19 20:21:17 -0600

    axios base url updated

--- src/axiosconfig.js
+++ src/axiosconfig.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: "http://127.0.0.1:10000/",
+  // baseURL: "http://127.0.0.1:10000/",
+  baseURL: "https://amazon-backend-yonas.cyclic.app",
 });
 
 export default instance;
